b84402a297ca76c82dda81ea2bc97b1d6acf6b9a
thanya <thanya@thanyas-MacBook-Pro.local>
2020-10-18 01:44:39 +0530

    copy of dunston backend

---
+++ public/precache-manifest.5c37aadf90e3e14c22d8c244c47fb3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b76569b8364eda4d285ef797264c350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "528fa26f94ad836a292b",
+    "url": "/static/js/2.79881295.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.79881295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fd463fe156d9470c94",
+    "url": "/static/js/main.ccfe8ecd.chunk.js"
+  },
+  {
+    "revision": "100658504a6d4965b7b5",
+    "url": "/static/js/runtime-main.faf8911e.js"
+  }
+]);
